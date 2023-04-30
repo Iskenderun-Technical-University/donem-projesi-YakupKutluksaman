4f3a2bd05dd3d60792fba33dbac1cb990939a2cc
yakup <yakupks789@gmail.com>
2023-04-30 23:30:36 +0300

    Giriş ekran tasarlandı

--- ALEL_Hastane_Otomasyonu_/ALEL_Hastane_Otomasyonu_/Form1.cs
+++ ALEL_Hastane_Otomasyonu_/ALEL_Hastane_Otomasyonu_/Form1.cs
@@ -16,5 +16,10 @@ namespace ALEL_Hastane_Otomasyonu_
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
